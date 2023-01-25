4c8a2519b88c0f8c4bd2d85b57eace81b3e47e34
Pakize Bozkurt <pakiozihak@gmail.com>
2023-01-25 13:28:57 +0000

    Data types

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
+// Declaring variables
+
 let js = "amazing";
-if (js === "amazing") alert("JavaScript is FUN!");
+//if (js === "amazing") alert("JavaScript is FUN!");
 console.log(40 + 8 + 23 - 10);
+
+// Try to descriptive variable
+
+let job1 = "Philosophy Teacher";
+let job2 = "Developer";
+
+console.log(job2);
+
+/// Values and Variables 
+
+let country = "Turkey";
+let continent = "Europe";
+let population = 85.000000;
+
+
+/// Data Types
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 25);
+console.log(typeof 'Angela');
+
+//Boolean True / False
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+
+
