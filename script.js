1d597ed2e0355159056d833de5c215f3bd2ec2fa
Pakize Bozkurt <pakiozihak@gmail.com>
2023-01-25 13:49:40 +0000

    Operators

--- script.js
+++ script.js
@@ -1,34 +1,53 @@
 // Declaring variables
 
-let js = "amazing";
+//let js = "amazing";
 //if (js === "amazing") alert("JavaScript is FUN!");
-console.log(40 + 8 + 23 - 10);
+//console.log(40 + 8 + 23 - 10);
 
 // Try to descriptive variable
 
-let job1 = "Philosophy Teacher";
-let job2 = "Developer";
+// let job1 = "Philosophy Teacher";
+// let job2 = "Developer";
 
-console.log(job2);
+// console.log(job2);
 
 /// Values and Variables 
 
-let country = "Turkey";
-let continent = "Europe";
-let population = 85.000000;
+// let country = "Turkey";
+// let continent = "Europe";
+// let population = 85.000000;
 
 
 /// Data Types
 
-console.log(typeof true);
-console.log(typeof javascriptIsFun);
-console.log(typeof 25);
-console.log(typeof 'Angela');
+// console.log(typeof true);
+// console.log(typeof javascriptIsFun);
+// console.log(typeof 25);
+// console.log(typeof 'Angela');
 
 //Boolean True / False
 
-let javascriptIsFun = true;
-console.log(javascriptIsFun);
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
+
+
+// Assignment operators
+
+x += 10; // x = x + 10 = 25
+x *= 4;  // x = x * 4 = 100
+x++;  // x = x + 1
+x--;  // x = x -1
+
+console.log(x);
+
+// Comparison operators
+
+let ageAlex = 59;
+let ageAngela = 61;
+
+console.log(ageAlex < ageAngela); // >, <, >=, <=
+
+const isFullName = ageAngela >= 48;
 
 
 
